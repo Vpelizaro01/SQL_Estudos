2ee67900227ad332503256fb737bb783eb3da2d5
Vinicius Pelizaro Ayres <vinipelizaro@hotmail.com>
2025-10-23 13:28:33 -0300

    Add files via upload

---
+++ projeto_retail_sales.sql
@@ -0,0 +1,77 @@
+-- Criação de tabela
+DROP TABLE IF EXISTS retail_sales;
+CREATE TABLE retail_sales
+            (
+                transaction_id INT PRIMARY KEY,	
+                sale_date DATE,	 
+                sale_time TIME,	
+                customer_id	INT,
+                gender	VARCHAR(15),
+                age	INT,
+                category VARCHAR(15),	
+                quantity	INT,
+                price_per_unit FLOAT,	
+                cogs	FLOAT,
+                total_sale FLOAT
+            );
+
+SELECT * FROM retail_sales
+LIMIT 10;
+SELECT COUNT(*) FROM retail_sales
+
+-- Limpeza de Dados 
+SELECT FROM retail_sales
+WHERE transactions_id IS NULL
+
+SELECT * FROM retail_sales
+WHERE sale_date IS NULL
+
+SELECT * FROM retail_sales
+WHERE sale_time IS NULL
+
+SELECT * FROM retail_sales
+WHERE 
+    transaction_id IS NULL
+    OR
+    sale_date IS NULL
+    OR 
+    sale_time IS NULL
+    OR
+    gender IS NULL
+    OR
+    category IS NULL
+    OR
+    quantity IS NULL
+    OR
+    cogs IS NULL
+    OR
+    total_sale IS NULL;
+--
+
+DELETE FROM retail_sales
+WHERE 
+    transaction_id IS NULL
+    OR
+    sale_date IS NULL
+    OR 
+    sale_time IS NULL
+    OR
+    gender IS NULL
+    OR
+    category IS NULL
+    OR
+    quantity IS NULL
+    OR
+    cogs IS NULL
+    OR
+    total_sale IS NULL;
+	-- Vendo os dados 
+	
+	SELECT COUNT(*) as total_sale FROM retail_sales
+	-- quantidade clientes 
+	
+	SELECT COUNT(DISTINCT customer_id) as total_sale FROM retail_sales
+	SELECT DISTINCT category FROM retail_sales
+
+
+	
